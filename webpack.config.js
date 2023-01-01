0dec1224100813c35f63fb014409a029649f2618
Connie <sitwailan@gmail.com>
2022-12-31 20:34:01 -0500

    change

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 module.exports = {
   mode: "production",
